24ff121fc4b7ceaa94bae3397d6b332f2150a301
Cédric Oloa <cedric.oloa@laposte.net>
2016-04-04 23:20:36 +0200

    modifications jumbotron+shadow

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 <script src="js/jquery-1.12.2.js"></script>
 <script src="js/classie.js"></script>
 <!-- Script pour rétrécir le header -->
-<script>  
+<script>
     function init() {
         window.addEventListener('scroll', function(e){
             var distanceY = window.pageYOffset || document.documentElement.scrollTop,
@@ -60,48 +60,52 @@
             <div class="triangle"></div>
         </div>
         <section class="bg-nuages test">
-            <div class="container ">
+            <div class="container jumbotron">
+                <br />
                 <h1>Le Challenge</h1>
-                <p>Le Challenge ENAC Alumni – Avico est un concours de création d’entreprise dans le domaine aéronautique. Il est porté par ENAC Alumni avec le soutien d’Avico, son partenaire fondateur.</p>
+                <br />
+                <p align="justify">Le Challenge ENAC Alumni – Avico est un concours de création d’entreprise dans le domaine aéronautique. Il est porté par ENAC Alumni avec le soutien d’Avico, son partenaire fondateur.</p>
 
-                <p>La 1ère édition du Challenge fut lancée en 2014, et devant le succès du Challenge et la qualité des projets reçus, les organisateurs ont décidé de lancer une 2nde édition du Challenge ENAC Alumni Avico en 2015. L’objectif commun d’ENAC Alumni et d’Avico est de récompenser les meilleurs projets innovants chaque année, de devenir un véritable laboratoire d’idées connu et reconnu.</p>
+                <p align="justify">La 1ère édition du Challenge fut lancée en 2014, et devant le succès du Challenge et la qualité des projets reçus, les organisateurs ont décidé de lancer une 2nde édition du Challenge ENAC Alumni Avico en 2015. L’objectif commun d’ENAC Alumni et d’Avico est de récompenser les meilleurs projets innovants chaque année, de devenir un véritable laboratoire d’idées connu et reconnu.</p>
 
-                <p>Nous souhaitons faire émerger des idées novatrices dans le domaine de l’aéronautique et stimuler la fibre entrepreneuriale chez les jeunes, moins jeunes, expérimentés ou néophytes qui se lanceraient dans l’aventure du Challenge.</p>
+                <p align="justify">Nous souhaitons faire émerger des idées novatrices dans le domaine de l’aéronautique et stimuler la fibre entrepreneuriale chez les jeunes, moins jeunes, expérimentés ou néophytes qui se lanceraient dans l’aventure du Challenge.</p>
 
-                <p>Le Challenge ENAC Alumni Avico est ouvert à tous à partir de 18 ans, sans distinction de formation, de parcours, ou de nationalité. Les Challengers peuvent se présenter seuls ou par équipe. Le jury aura à cœur de sélectionner et promouvoir le meilleur projet, les dossiers présentés par des anciens élèves de l’ENAC ne feront l’objet d’aucune préférence.</p>
+                <p align="justify">Le Challenge ENAC Alumni Avico est ouvert à tous à partir de 18 ans, sans distinction de formation, de parcours, ou de nationalité. Les Challengers peuvent se présenter seuls ou par équipe. Le jury aura à cœur de sélectionner et promouvoir le meilleur projet, les dossiers présentés par des anciens élèves de l’ENAC ne feront l’objet d’aucune préférence.</p>
 
                 <p>Les Challengers seront également invités à participer au « coup de cœur du web », un concours dans le concours qui permet d’exposer son projet au grand public et de bénéficier du support d’une plateforme de crowdfunding.</p>
                 <div class="button">
                     <a href="#" class="btn-medium">Je participe</a>
                 </div>
+                <br />
             </div>
 
         </section>
          <div id="jury"></div>
-        <section class="color">
-            <div class="container">
+        <section>
+            <div class="container jumbotron">
                 <h1>Le Jury</h1>
                 <p>Le Jury du Challenge ENAC Alumni - Avico est composé de professionnels de l'aéronautique et de la finance et des organisateurs, des personnalités sensibles à l'innovation, à la créativité et surtout au courage et à la ténacité dont il faut faire preuve pour monter son projet entrepreneurial.</p>
                 <p>Tout au long du Challenge, ils auront à coeur de séléctionner les idées et les business plans pour faire émerger le meilleur d'entre eux. Sur la base de critères objectifs, le Jury désignera le projet qui l'aura séduit tant par son audace que sa faisabilité.</p>
                 <div class="row">
-                	<div class="col-xs-2"><img src="img/patrickbaudis.jpg" alt="jury1"></div>
-                	<div class="col-xs-2"><img src="img/patrickbaudis.jpg" alt="jury1"></div>
-                    <div class="col-xs-2"><img src="img/patrickbaudis.jpg" alt="jury1"></div>
-                    <div class="col-xs-2"><img src="img/patrickbaudis.jpg" alt="jury1"></div>
-                	<div class="col-xs-2"><img src="img/patrickbaudis.jpg" alt="jury1"></div>
-                    <div class="col-xs-2"><img src="img/patrickbaudis.jpg" alt="jury1"></div>
+                  	<div class="col-xs-2"><img src="img/patrickbaudis.jpg" alt="jury1" class="shadow"></div>
+                  	<div class="col-xs-2"><img src="img/patrickbaudis.jpg" alt="jury1" class="shadow"></div>
+                    <div class="col-xs-2"><img src="img/patrickbaudis.jpg" alt="jury1" class="shadow"></div>
+                    <div class="col-xs-2"><img src="img/patrickbaudis.jpg" alt="jury1" class="shadow"></div>
+                  	<div class="col-xs-2"><img src="img/patrickbaudis.jpg" alt="jury1" class="shadow"></div>
+                    <div class="col-xs-2"><img src="img/patrickbaudis.jpg" alt="jury1" class="shadow"></div>
                 </div>
+                <br />
             </div>
         </section>
         <div id="tuteur"></div>
         <section>
-            <div class="container">
+            <div class="container jumbotron">
                 <h1>Les Tuteurs</h1>
                 <p>Les Challengers pourront s’appuyer sur les conseils des tuteurs du Challenge pour préparer leur Business Plan et nouer de nouveaux contacts professionnels.</p>
                 <p>Une cinquantaine de tuteurs, issus d’entreprises de transport aérien, de l’industrie aéronautique, du monde financier, des services officiels, membres de l’Enac Alumni Executive Club, du Club Entrepreneur, apporteront leur appui aux Challengers pendant la durée du concours.</p>
                 <p>Les tuteurs ne sont pas membres du jury et l’accompagnement des Challengers n’est pas obligatoire.</p>
             </div>
-        </section>        
+        </section>
     </div>
 <!-- #main -->
 
