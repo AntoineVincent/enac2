8201359c37261369946c99802ed936a8edb5b3ee
Antoine <anton51200@laposte.net>
2016-04-12 16:59:39 +0200

    new header

--- header2.php
+++ header2.php
@@ -1,12 +1,25 @@
-<div class="clearfix navigation">
-    <h1 id="logo">Enac Alumni</h1>
-    <nav>
-
-        <a href="./index.php#challenge">Challenge</a>
-        <a href="./index.php#jury">Jury</a>
-        <a href="./index.php#tuteur">Tuteur</a>
-        <a href="./presse.php">Presse</a>
-        <a href="./faq.php">FAQ</a>
-        <a href="#ancrecontact" id="contact">Contact</a>
-   </nav>
-</div>
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="true" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Challenge Enac-Alumni Avico</a>
+        </div>
+        <div style="" aria-expanded="true" id="navbar" class="navbar-collapse collapse in">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php#challenge">Challenge</a></li>
+            <li><a href="index.php##jury">Jury/Tuteur</a></li>
+            <li><a href="">Actus</a></li>
+            <li><a href="">Participer</a></li>
+            <li><a href="index.php##partenaires">Nos Partenaires</a></li>
+            <li><a href="./presse.php">Presse</a></li>
+            <li><a href="./faq.php">FAQ</a></li>
+            <li><a href="#ancrecontact" id="contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
