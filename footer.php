38431a20ee8db214d235e6c8c54b434b55b1384c
Antoine <anton51200@laposte.net>
2016-04-18 09:38:01 +0200

    q

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-xs-12 col-md-3">
-		<p class="copyb"> Site construit par les élèves de la Wild code school: </p>
+		<p class="copyb"> Site développé par les élèves de la Wild code school: </p>
 	</div>
 	<div class="col-xs-12 col-md-3">
 		<p class="copyb"> Antoine / Augustin / Cédric </p>
